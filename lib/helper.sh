4090e64731d8a237327895bde1bd73c9f78a50bc
John Valai <git@jvk.to>
2021-08-17 01:00:45 +0200

    Move bash colours/styles to separate file

---
+++ lib/helper.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ANSI/VT100 colours
+YELLOW='\033[1;33m'
+LIGHTYELLOW='\033[0;33m'
+RED='\033[0;31m'
+LIGHTRED='\033[1;31m'
+GREEN='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+BLUE='\033[0;34m'
+LIGHTBLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LIGHTPURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+BOLD="\033[1m"
+INVERT="\033[7m"
+RESET='\033[0m'
+
+# Some preset styles
+S_NORM="${WHITE}"
+S_LIGHT="${LIGHTGRAY}"
+S_NOTICE="${GREEN}"
+S_QUESTION="${YELLOW}"
+S_WARN="${LIGHTRED}"
+S_ERROR="${RED}"
+
+# Notification icons
+I_OK="✅"; 
+I_TIME="⏳"; 
+I_STOP="🚫"; 
+I_ERROR="❌"; 
+I_END="👋🏻"
